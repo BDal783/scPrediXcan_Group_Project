812aceb3022dc47443144286e9f83c95b691b09e
JudePilt2 <jpiltingsrud@luc.edu>
2025-04-01 15:03:48 -0500

    update

--- Pipe.py
+++ Pipe.py
@@ -1,7 +1,6 @@
 import sys, os 
 import argparse
 import difflib
-<<<<<<< HEAD
 import gzip
 
 # function to parse command line arguments
@@ -34,34 +33,6 @@ known_columns = ["snp_column", "effect_allele_column", "non_effect_allele_column
 
 # GWAS headers to match
 gwas_headers = headers_list 
-=======
-
-#function to parse command line arguments
-# def check_arg(args=None):
-#     parser = argparse.ArgumentParser(
-#     description="ADD TITLE OF SCRIPT HERE (shows on help -h)")
-#     parser.add_argument("-i", "--input",
-#     help="input file",
-#     required=True)
-#     parser.add_argument("-o", "--output",
-#     help="output file",
-#     required=True)
-#     return parser.parse_args(args)
-
-# #retrieve command line arguments
-# arguments = check_arg(sys.argv[1:])
-# infile = arguments.input
-# outfile = arguments.output
-
-
-#known column names
-known_columns = ["snp_column", "effect_allele_column", "non_effect_allele_column", "reference_allele_column", "other_allele_column", "alleles_column", "beta_column", "pvalue_column", "se_column", "freq_column", "position_column", "chromosome_column", 'locus_column', "No match found" ]
-
-#GWAS headers to match
-gwas_headers = ["CHR", "BP", "RS", "SNP_hg38", "A1", "A2", "FREQ", "BETA", "SE", "P", 
- "locus", "how", "ID", "REF", "ALT", "#CHROM", "POS", 
- "Pvalue", "AF_Allele2", "alleles", "#CHROM", "Pvalue"]
->>>>>>> 0868a33121473d109c776ee161f6aee6d33400e8
 
 explicit_mappings = {
     "P": "pvalue_column",
@@ -83,11 +54,7 @@ explicit_mappings = {
     'how': 'No match found',
     }
 
-<<<<<<< HEAD
 # mapping headers to closest known column names
-=======
-#mapping headers to closest known column names
->>>>>>> 0868a33121473d109c776ee161f6aee6d33400e8
 mapped_columns = {}
 for header in gwas_headers:
     if header in explicit_mappings:
@@ -99,24 +66,48 @@ for header in gwas_headers:
         else:
             mapped_columns[header] = "No match found"
 
-<<<<<<< HEAD
 # print the mapping
 print(mapped_columns)
 
-# commands that use the dicionary of mapped columns  to assign header name:
-# example: 
-# snp_column SNP 
-# effect_allele_column A1
-# non_effect_allele_column A2
-# then use os to call SPrediXcan.py with the necessary info/info from the GWAS
-# basic info will include: SNP, effect allele, non effect allele, beta, pval. 
-# and hope it works :) 
+def run_bash(command): 
+    print(f"Running command: {command}")
+    os.system(command)
 
-=======
-# Print the mapping
-print(mapped_columns)
+# using the mapping to create the command that calls SPrediXcan;
+command = ""
 
+# Iterate through all known columns and find the corresponding headers
+for known_column in known_columns:
+    matching_header = [key for key, value in mapped_columns.items() if value == known_column]
+    if matching_header and known_column != "No match found":  # exclude "No match found"
+        command += f"--{known_column} {matching_header[0]} \\\n"
 
+# Print or execute the command
+print(f"Generated command:\n{command}")
+run_bash(command)
 
-#Reading in the results and creating a manhatten plot of the results section -Brendon
->>>>>>> 0868a33121473d109c776ee161f6aee6d33400e8
+# main command that calls SPrediXcan.py and gives path info for models and data - taken from github
+main_c = (
+    "./SPrediXcan.py \\\n"
+    "--model_db_path data/DGN-WB_0.5.db \\\n"
+    "--covariance data/covariance.DGN-WB_0.5.txt.gz \\\n"
+    "--gwas_folder data/GWAS \\\n"
+    "--gwas_file_pattern \".*gz\" \\\n"
+)
+
+# make the column info portion of the command using mapping from above
+info_c = ""
+for known_column in known_columns:
+    matching_header = [key for key, value in mapped_columns.items() if value == known_column]
+    if matching_header and matching_header[0] != "No match found":
+        info_c += f"--{known_column} {matching_header[0]} \\\n"
+
+# final output location
+output_c = "--output_file {outfile}"
+
+# Combine all parts into the full command
+command = main_c + info_c + output_c
+
+# Print or execute the full command
+print(f"Generated full command:\n{command}")
+run_bash(command)
